b6a895ca4514ede1dbb9121b8bc7bdbcbec6593f
simona8989 <pischeddasimona.dev@gmail.com>
2025-03-04 10:55:36 +0100

    modifiche

--- dist/weather-app/server/angular-app-manifest.mjs
+++ dist/weather-app/server/angular-app-manifest.mjs
@@ -29,11 +29,11 @@ export default {
 ],
   entryPointToBrowserMapping: undefined,
   assets: {
-    'index.csr.html': {size: 27860, hash: 'e77f4c94125132613952f301ba3b328983bc8c45a2de0bde93212ebfdbb866ef', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
-    'index.server.html': {size: 17143, hash: '0212e96620f4f88ec5514c2a65dbd9a88ea37e022e7b571eeb1ca57f2941d6d1', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
-    'home/index.html': {size: 42108, hash: 'a47402c84fe66fddded871f9671c253dc0ec7d7155a223173838d61df0eafa5c', text: () => import('./assets-chunks/home_index_html.mjs').then(m => m.default)},
-    'forecast/index.html': {size: 41853, hash: 'f92c304acabe07ef7fcb65d44db3a3e5157c78e00744d86404434006b8f8c527', text: () => import('./assets-chunks/forecast_index_html.mjs').then(m => m.default)},
-    'not-found/index.html': {size: 41152, hash: '5774f9b8067c8b2627dd66c75f331507fa17bc46bec3dac1384d7568954d01bb', text: () => import('./assets-chunks/not-found_index_html.mjs').then(m => m.default)},
+    'index.csr.html': {size: 27875, hash: 'b3e8f4acdaab01da7bc53e3d5ac89f9d0bc8284efa235b44d8a385fb4de98c8d', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
+    'index.server.html': {size: 17158, hash: '812d9e5754462f46d12735c13630669b15bc1d0cc2ca32c3c3bfc2293ad092fc', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
+    'forecast/index.html': {size: 41868, hash: 'bcffc8a885685d3d642897a889ea312d0602d4619af549e350523358e3ce07a2', text: () => import('./assets-chunks/forecast_index_html.mjs').then(m => m.default)},
+    'not-found/index.html': {size: 41167, hash: '01acc73f67e1bae5e1679718dd54c730baf4d1907defb46c60eeb234474bc03e', text: () => import('./assets-chunks/not-found_index_html.mjs').then(m => m.default)},
+    'home/index.html': {size: 42123, hash: '8767ebe6919c3322b1c37fcf3c7fe5731f3f68c824987eb482cd237caddf4941', text: () => import('./assets-chunks/home_index_html.mjs').then(m => m.default)},
     'styles-CHA4UZZU.css': {size: 322269, hash: '7S2+NUtTxgM', text: () => import('./assets-chunks/styles-CHA4UZZU_css.mjs').then(m => m.default)}
   },
 };
